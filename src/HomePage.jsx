19863c4f9b8c515d8cb65d0fa1b65032f6caf4cf
mahmutunder <mahmutunder@gmail.com>
2025-02-14 21:51:37 -0700

    fix css

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -4,9 +4,9 @@ function HomePage() {
     <div>
       <h1>Welcome to my page</h1>
       <div>
-        <Link to="/html">HTML</Link>
-        <Link to="/js">JS</Link>
-        <Link to="/react">React</Link>
+        <Link to="/html" className="link">HTML</Link>
+        <Link to="/js" className="link">JS</Link>
+        <Link to="/react" className="link">React</Link>
       </div>
     </div>
   );
